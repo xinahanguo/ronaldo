623941e49ada2456dd45fc4352999b77869e8a0f
wcx <distinctofwang@gmail.com>
2015-10-26 18:32:32 +0800

    sencond

---
+++ Ronaldo/app/src/main/java/com/ronaldo/wcx/ronaldo/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.ronaldo.wcx.ronaldo;
+
+/**
+ * Created by wcx on 2015/10/26.
+ */
+public class BaseActivity extends BaseAppCompatActivity{
+}
---
+++ Ronaldo/app/src/main/java/com/ronaldo/wcx/ronaldo/BaseAppCompatActivity.java
@@ -0,0 +1,19 @@
+package com.ronaldo.wcx.ronaldo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wcx on 2015/10/26.
+ *
+ * Think Android
+ *  是一个免费的开源的、简易的、遵循Apache2开源协议发布的Android开发框架，其开发宗旨是简单、快速的进行Android应用程序的开发，
+ *  包含Android  mvc、简易sqlite orm、ioc模块、封装Android  httpclitent的http模块,具有快速构建文件缓存功能，无需考虑缓存文件的格式，
+ *  都可以非常轻松的实现缓存，它还基于文件缓存模块实现了图片缓存功能，在android中加载的图片的时候，
+ *  对oom的问题，和对加载图片错位的问题都轻易解决。他还包括了一个手机开发中经常应用的实用工具类，如日志管理，配置文件管理，android下载器模块，
+ *  网络切换检测等等工具。
+ *
+ * Android butterknife  //注解框架
+ */
+public class BaseAppCompatActivity extends AppCompatActivity {
+
+}
